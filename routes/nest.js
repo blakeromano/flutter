e83903f8d1e903e00644a8ddb904abc498841e05
blakeromano19 <blakeromano19@gmail.com>
2021-07-23 14:50:34 -0400

    index profiles, navbar nest

--- routes/nest.js
+++ routes/nest.js
@@ -6,16 +6,17 @@ export {
 
 const router = Router()
 
-router.get("/", isLoggedIn, nestCtrl.index)
-router.get("/:id", isLoggedIn, nestCtrl.showProfile)
-router.put("/:id", isLoggedIn, nestCtrl.updateProfile)
+router.get("/", isLoggedIn, nestCtrl.index) // done
+router.get("/allProfiles", isLoggedIn, nestCtrl.indexProfiles) // done
+router.get("/:id", isLoggedIn, nestCtrl.showProfile) // done
+router.put("/:id", isLoggedIn, nestCtrl.updateProfile) // done
 router.delete("/:id", isLoggedIn, nestCtrl.deleteProfile)
-router.post("/flocks", isLoggedIn, nestCtrl.newFlock)
-router.post("/posts", isLoggedIn, nestCtrl.newPost)
-router.get("/posts", isLoggedIn, nestCtrl.indexPosts)
+router.post("/flocks", isLoggedIn, nestCtrl.newFlock) // done
+router.post("/posts", isLoggedIn, nestCtrl.newPost) // done
+router.get("/posts", isLoggedIn, nestCtrl.indexPosts) 
 router.get("/posts/:id", isLoggedIn, nestCtrl.showPost)
-router.put("/posts/:id", isLoggedIn, nestCtrl.updatePost)
-router.delete("/posts/:id", isLoggedIn, nestCtrl.deletePost)
+router.put("/posts/:id", isLoggedIn, nestCtrl.updatePost) //done
+router.delete("/posts/:id", isLoggedIn, nestCtrl.deletePost) //done
 router.post("/posts/:id/like", isLoggedIn, nestCtrl.likePost)
 
 
